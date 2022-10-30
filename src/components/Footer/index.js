0150745f577780f01b99a8c4cf6662b6c557d126
Sai Bingi <saibingi1208@gmail.com>
2022-10-30 15:16:45 +0000

    Layout Builder App Solution

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-bg-container">
+    <div className="footer-container">
+      <h1 className="footer">Footer</h1>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const Header = () => (
+  <div className="header-bg-container">
+    <div className="header-container">
+      <h1 className="header">Header</h1>
+    </div>
+  </div>
+)
+
+export default Header
